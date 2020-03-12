4305d35d437f201bdfee3cc215f5244b8c6b8115
MORIN Maxence <astraxalumix@gmail.com>
2020-03-12 12:22:45 +0100

    interclassement

--- file.h
+++ file.h
@@ -37,3 +37,20 @@ void affiche_file(char *msg, File f);
  * Renvoie la dernière occurence du max de la file. NULL si la file est vide
  */
 Cellule * max_file(File f);
+
+/**
+ * Renvoie les dernière occurence du min et du max. 
+ * Ne change pas pmin et pmpax si la file est vide.
+ */
+void min_et_max_file(File f, Cellule **pmin, Cellule **pmax);
+
+/**
+ * Une  fonction  d’interclassementde  deux  files f1etf2triées  
+ * en  ordre  croissant  avec suppression des doublons.
+ * Par exemple, si f1contient 1 2 4 6 7 8 9 et f2contient 
+ * 1 3 5 6, le résultat sera 1 2 3 4 5 6 7 8 9. 
+ * Votre fonction doit marcher quel que soit l’état 
+ * (vide / non vide) des deux files. 
+ * Ces dernières ne doivent pas être modifiées.
+ */
+File interclassement(File f1, File f2);
--- main.c
+++ main.c
@@ -29,10 +29,52 @@ void test_max_file(){
     printf("max_value %d\n", max_cell->valeur);
 }
 
+void test_min_et_max_file(){
+    File f = file_vide();
+    f = enfiler(1, f);
+    f = enfiler(6, f);
+    f = enfiler(7, f);
+    f = enfiler(5, f);
+    f = enfiler(7, f);
+    f = enfiler(1, f);
+
+    Cellule *pmin, *pmax;
+
+    min_et_max_file(f, &pmin, &pmax);
+
+    printf("min : %d max : %d\n", pmin->valeur, pmax->valeur);
+}
+
+void test_interclassement(){
+    File f1 = file_vide();
+    File f2 = file_vide();
+    f1 = enfiler(1, f1);
+    f1 = enfiler(2, f1);
+    f1 = enfiler(4, f1);
+    f1 = enfiler(6, f1);
+    f1 = enfiler(7, f1);
+    f1 = enfiler(8, f1);
+    f1 = enfiler(9, f1);
+
+    f2 = enfiler(1, f2);
+    f2 = enfiler(3, f2);
+    f2 = enfiler(5, f2);
+    f2 = enfiler(6, f2);
+    
+    File result = interclassement(f1, f2);
+    
+    affiche_file("YOUPI : ", result);
+
+}
+
 int main(int argc, char** args){
-    //test_fonctionnement();
+    // test_fonctionnement();
 
-    test_max_file();
+    // test_max_file();
 
+    // test_min_et_max_file();
+    
+    test_interclassement();
+    
     return 0;
 }
